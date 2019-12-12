9a535c582a078edcec7059df16d2e07f9b75d163
Diego Perez <diegopm2000@gmail.com>
2019-12-12 20:14:25 +0100

    minor fix

--- 08-C-ExpressOpenApi-Calculator-OpenApi3-enhanced/calculator.controller.js
+++ 08-C-ExpressOpenApi-Calculator-OpenApi3-enhanced/calculator.controller.js
@@ -14,43 +14,36 @@ const log = require('./logger');
 
 function add(req, res) {
   const { a, b } = req.query;
-
   log.debug(`${MODULE_NAME}:add (IN) --> a: ${a}, b: ${b}`);
 
   const result = a + b;
 
   log.debug(`${MODULE_NAME}:add (OUT) --> result: ${result}`);
-
   res.send({ result });
 }
 
 function substract(req, res) {
   const { a, b } = req.query;
-
   log.debug(`${MODULE_NAME}:substract (IN) --> a: ${a}, b: ${b}`);
 
   const result = a - b;
 
   log.debug(`${MODULE_NAME}:substract (OUT) --> result: ${result}`);
-
   res.send({ result });
 }
 
 function multiply(req, res) {
   const { a, b } = req.query;
-
   log.debug(`${MODULE_NAME}:multiply (IN) --> a: ${a}, b: ${b}`);
 
   const result = a * b;
 
   log.debug(`${MODULE_NAME}:multiply (OUT) --> result: ${result}`);
-
   res.send({ result });
 }
 
 function divide(req, res) {
   const { a, b } = req.query;
-
   log.debug(`${MODULE_NAME}:division (IN) --> a: ${a}, b: ${b}`);
 
   // Check division by zero
