fc50525c1d1d9af1d2aab15412411523d84d3ee4
Diego Perez <diegopm2000@gmail.com>
2019-12-14 09:20:20 +0100

    minor fixes

--- 09-ExpressOpenApi-User-Management-NLayer/api/controllers/user.controller.js
+++ 09-ExpressOpenApi-User-Management-NLayer/api/controllers/user.controller.js
@@ -2,23 +2,31 @@
 
 function createUser(req, res) {
   // TODO
+  console.log('Entrando en createUser...');
+  res.json({ rresult: 'Not built yet!' });
 }
 
 function updateUser(req, res) {
   // TODO
+  console.log('Entrando en updateUser...');
+  res.json({ rresult: 'Not built yet!' });
 }
 
 function getUsers(req, res) {
   console.log('Entrando en getUsers...');
-  // TODO
+  res.json({ rresult: 'Not built yet!' });
 }
 
 function getUserById(req, res) {
   // TODO
+  console.log('Entrando en getUserById...');
+  res.json({ rresult: 'Not built yet!' });
 }
 
 function deleteUser(req, res) {
   // TODO
+  console.log('Entrando en deleteUser...');
+  res.json({ rresult: 'Not built yet!' });
 }
 
 module.exports = {
