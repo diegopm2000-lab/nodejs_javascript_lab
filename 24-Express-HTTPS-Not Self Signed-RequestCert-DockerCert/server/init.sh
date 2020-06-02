0ba4dfb1a38e6903c79a55cc21a905aed428d6be
Diego Perez <diegopm2000@gmail.com>
2020-06-02 20:33:31 +0200

    testing

---
+++ 24-Express-HTTPS-Not Self Signed-RequestCert-DockerCert/server/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export PORT="8443"
+env | grep '^PORT='
+
+export KEY=`cat ../certs/localhost/server-key.pem`
+env | grep '^KEY='
+
+export CERT=`cat ../certs/localhost/server-crt.pem`
+env | grep '^CERT='
+
+export CA=`cat ../certs/localhost/ca-crt.pem`
+env | grep '^CA='
+
+
